1c2a626fa30917753b2dc00f29d28ab308b61aa8
Koko Hranghlu <hranghlukoko@gmail.com>
2025-05-21 19:30:24 +0530

    add basic faq accordion

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
+// back to top button
 $('.top-btn').click(function () {
   $(window).scrollTop(0);
 });
-// add contact modal (done)
-// design the modal better
+
+// modal
 $(".mail-btn").click(function(e) {
   e.preventDefault();
-  $(".modal").css("display", "block")
+  $(".modal").css("display", "grid")
 });
 
 $(".modal-close-btn").click(() => {
   $(".modal").css("display", "none")
 })
+
+// faq
+$(".item__btn").click((e) => {
+  const item = e.target.closest(".item");
+  const item_body = $(item).find(".item__body");
+  item_body.slideToggle();
+});
